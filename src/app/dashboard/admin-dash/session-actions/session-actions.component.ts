e34b6c55f42dcefda0070f8d47fb3cc5a211c908
FEvelina <128091832+FEvelina@users.noreply.github.com>
2025-06-27 16:54:59 +0300

    notes:  - cancel button still needs work  - session: how shall the add/edit dialog be transformed and added in the course form

---
+++ src/app/dashboard/admin-dash/session-actions/session-actions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-actions',
+  imports: [],
+  templateUrl: './session-actions.component.html',
+  styleUrl: './session-actions.component.css'
+})
+export class SessionActionsComponent {
+
+}
